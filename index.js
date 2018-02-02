e110350ec5347fb39b1c593d62d8dfc031d2605c
Christopher Chang <chris@mlab.com>
2018-02-01 19:40:21 -0800

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('Development server');
+  res.status(200).send('Testing CodePipeline');
 });
 
 // There will be a test page available on the /test path of your server url
